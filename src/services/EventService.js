e0197614c171403154c78ed0ec8253183bb1279a
supachok jrirarojkul <supachok8728@gmail.com>
2022-09-15 23:11:00 +0700

    8.6

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -11,9 +11,9 @@ const apiClient = axios.create({
 
 export default {
   getEvents(perPage, page) {
-    return apiClient.get('/events?_limit=' + perPage + '&_page=' + page)
+    return apiClient.get('/event?_limit=' + perPage + '&_page=' + page)
   },
   getEvent(id) {
-    return apiClient.get('/events/' + id)
+    return apiClient.get('/event/' + id)
   }
 }
